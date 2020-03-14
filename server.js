7be81b1d0728e3cf3608a3d64c52062365d5adc4
Paul Nissen <nissenbrau@gmail.com>
2020-03-14 16:06:41 -0600

    added cors and helmet dependencies

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 const POKEDEX = require('./pokedex.json');
-
-console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan('dev'));
-
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization')
   const apiToken = process.env.API_TOKEN
-
-  console.log('validate bearer token middleware')
-
+  // validate bearer token 
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-
+  // move to next middleware
   next()
 })
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
 function handleGetTypes(req, res) {
   res.json(validTypes);
@@ -33,7 +33,9 @@ app.get('/types', handleGetTypes)
 function handleGetPokemon(req, res) {
   const { name, type } = req.query
   let results = POKEDEX.pokemon
+  // filter by type if type query parameter is present
   if (type) {
+    // make sure user submits valid pokemon type
     if (!validTypes.includes(type)) {
       return res
         .status(400)
@@ -41,11 +43,11 @@ function handleGetPokemon(req, res) {
     }
     results = results.filter(pokemon => pokemon.type.includes(type))  
   }
-  
+  // filter by name if name query parameter is present, convert all to lowercase so results are case insensitive
   if (name) {
     results = results.filter(pokemon => pokemon.name.toLowerCase().includes(name.toLowerCase()))
   }
-
+  // message user if their search parameters yielded 0 results
   if (results.length === 0) {
       res.send("Sorry, there were 0 results for your search. Please try a different search query")
     }
